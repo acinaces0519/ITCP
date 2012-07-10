f2ca9697ea3588d12e48c8235629eb8cc26dc2a7
nakagawa <nakagkawaitcp@gmail.com>
2012-07-10 17:55:10 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ITCP::Application.config.secret_token = '8f2c5112b36a605e8b2cf2dd594181ec7cb607ffd12610f6b4779ff15572e3c0f5b21eb107ede6af216f669b53437801cbc764d83c8c529fb2f581f1d3c6f153'
